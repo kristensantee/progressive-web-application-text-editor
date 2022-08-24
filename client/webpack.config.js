3061f7ea78872c5655d16218d76e740c02ed550b
kristensantee <mikeandkris27@gmail.com>
2022-08-23 18:05:45 -0700

    updated weback url and public path

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,8 +34,8 @@ module.exports = () => {
         description: 'PWA practice',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
